028271e8abab82bd9cf0412a9919be2d1b3607db
andyL09 <alawrence15@cps.edu>
2025-05-14 11:18:02 -0500

    i love forgetting to commit things

--- a8.py
+++ a8.py
@@ -4,4 +4,10 @@ print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
 
 
+print("\n\nTraining XOR\n\n")
+xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
+
+xorn = NeuralNet(2, 4, 1)
+xorn.train(xor_training_data)
+print(xorn.test_with_expected(xor_training_data))
 
